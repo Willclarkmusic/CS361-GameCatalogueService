9a92a3678638f3116c6521b33ad5a7c9c66ef1db
Prismo <willclarkmusic@gmail.com>
2025-11-14 19:12:19 -0800

    cors

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import routes.gamesRoutes as routes
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://localhost:4000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(routes.router)
 
 @app.get("/")
